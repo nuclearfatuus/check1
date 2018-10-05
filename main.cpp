c5d1891ab03b31eada6bfe66ce209d3de25cc042
nuclearfatuus <nuclearfatuus@gmail.com>
2018-10-05 14:48:11 +0300

    2task

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 
 int main() {
-    int a = 3;
-    a *= 6;
-    std::cout << a << std::endl;
-    a /= 3;
-    a -= 12;
 
-    std::cout << a << std::endl;
+    int b = 128789;
+    short int c = 125;
+    int d = 32768;
+    float e = 1000,5;
+
+    std::cout << b << std::endl;
+    std::cout << c << std::endl;
+    std::cout << d << std::endl;
+    std::cout << e << std::endl;
+
     return 0;
 }
