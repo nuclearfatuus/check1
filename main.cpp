319f9902495630294bd5c3c6624ae698943f44c9
nuclearfatuus <nuclearfatuus@gmail.com>
2018-10-05 15:28:18 +0300

    4task

--- main.cpp
+++ main.cpp
@@ -2,15 +2,56 @@
 
 int main() {
 
-    int a = 2091;
-    int b = a/a;
+    int a = 1902;
+    int b = 0;
+
+    if (a/1000 >= 0) {
+        if (a/1000 > 0){
+            b += 1000*(a/1000 - 1);
+        }
+        else
+            b += 1000*(10-1);
+    }
+    else
+        std::cout << "neg" << std::endl;
+
+    a %= 1000;
+
+    if (a/100 >= 0) {
+        if (a/100 > 0){
+            b += 100*(a/100 -1);
+        }
+        else
+            b += 100*(10-1);
+    }
+    else
+        std::cout << "neg" << std::endl;
+
+    a %= 100;
+
+    if (a/10 >= 0) {
+        if (a/10 > 0) {
+            b += 10*(a/10 -1);
+        }
+        else
+            b += 10*(10-1);
+    }
+    else
+        std::cout << "neg" << std::endl;
+
+    a %= 10;
+
+    if (a/1 >= 0) {
+        if (a/1 >0) {
+            b += 1*(a/1 - 1);
+        }
+        else
+            b += 1*(10-1);
+    }
+    else
+        std::cout << "neg" << std::endl;
+
     std::cout << b << std::endl;
-    int k = (a - (1000+100))/100;
-    std::cout << k << std::endl;
-    int z = a-a;
-    std::cout << z << std::endl;
-    int x = a/1000;
-    std::cout << x << std::endl;
 
     return 0;
 }
